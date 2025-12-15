4a7b4bda288af964ef947e6a9460b107da554f39
Sergi Ortiz Gómez <drualcman@msn.com>
2025-12-15 12:18:51 +0800

    Add new parameter Gab to the ColumnsBarChartParams

--- BlazorBasics.Charts/Models/ColumnsBarChartParams.cs
+++ BlazorBasics.Charts/Models/ColumnsBarChartParams.cs
@@ -2,14 +2,19 @@
 
 public class ColumnsBarChartParams
 {
-    public ColumnsBarChartParams(string backgroundColour = "#D3D3D3", int thickness = 20, int dimension = 100,
+    public ColumnsBarChartParams(
+        string backgroundColour = "#D3D3D3",
+        int thickness = 20,
+        int dimension = 100,
         bool showValues = false,
-        IEnumerable<ChartColor> chartColours = null)
+        IEnumerable<ChartColor> chartColours = null,
+        int gap = 5)
     {
         BackgroundColour = backgroundColour;
         Thickness = thickness;
         Dimension = dimension;
         ShowValues = showValues;
+        Gap = gap;
         ChartColors = new(chartColours ?? ChartColourHelper
             .InitializeColours(256, 30));
     }
@@ -17,6 +22,7 @@ public class ColumnsBarChartParams
     public string BackgroundColour { get; init; }
     public int Thickness { get; init; }
     public int Dimension { get; init; }
+    public int Gap { get; init; }
     public bool ShowValues { get; init; }
     public List<ChartColor> ChartColors { get; set; }
     public int MaxColours => ChartColors.Count;
